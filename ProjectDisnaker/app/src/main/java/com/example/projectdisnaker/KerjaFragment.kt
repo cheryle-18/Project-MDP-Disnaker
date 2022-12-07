ce2bed286903b31d7fa4171cb78e7c9b681a1f6b
Cherilyn Eugenia <cherilyn.eugenia@gmail.com>
2022-12-07 14:34:16 +0700

    Tampilan peluang kerja + detail

---
+++ ProjectDisnaker/app/src/main/java/com/example/projectdisnaker/KerjaFragment.kt
@@ -0,0 +1,33 @@
+package com.example.projectdisnaker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projectdisnaker.databinding.FragmentKerjaBinding
+
+class KerjaFragment : Fragment() {
+    private lateinit var binding: FragmentKerjaBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentKerjaBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textView32.setOnClickListener {
+            val fragment = DetailKerjaFragment()
+            val bundle = Bundle()
+            fragment.arguments = bundle
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+        }
+    }
+}
