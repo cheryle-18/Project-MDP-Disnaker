8b5a1fc5d77ae071e944a80bbc03ab37a98d1b65
jlairlangga <john_la@mhs.istts.ac.id>
2023-01-04 00:44:06 +0700

    a

--- ProjectDisnaker/app/src/main/java/com/example/projectdisnaker/perusahaan/PerusahaanProfileFragment.kt
+++ ProjectDisnaker/app/src/main/java/com/example/projectdisnaker/perusahaan/PerusahaanProfileFragment.kt
@@ -5,13 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projectdisnaker.R
+import com.example.projectdisnaker.api.UserResponseItem
 import com.example.projectdisnaker.databinding.FragmentPerusahaanProfileBinding
 
 class PerusahaanProfileFragment : Fragment() {
     private lateinit var binding: FragmentPerusahaanProfileBinding
-
+    private lateinit var user: UserResponseItem
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,12 +32,23 @@ class PerusahaanProfileFragment : Fragment() {
         actionBar?.setTitle("Disnaker")
         actionBar?.setDisplayHomeAsUpEnabled(false)
         setHasOptionsMenu(false)
+        user = (activity as PerusahaanActivity).user
 
         binding.layoutUbahPassPerusahaan.setOnClickListener {
             val fragment = PerusahaanPasswordFragment()
             requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container_perusahaan, fragment).commit()
         }
 
+        binding.btnEditPerusahaan.setOnClickListener{
+            if (binding.etAlamatPerusahaanProfile.text.toString().isBlank()&&
+                    binding.etTelpPerusahaanProfile.text.toString().isBlank()&&
+                    binding.etEmailPerusahaanProfile.text.toString().isBlank()){
+                Toast.makeText(requireActivity(), "Error", Toast.LENGTH_SHORT).show()
+            }
+            else{
+
+            }
+        }
         binding.layoutLogoutPerusahaan.setOnClickListener {
             requireActivity().finish()
         }
